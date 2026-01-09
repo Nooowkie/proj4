49697beefeb048addeb601054045591c10178278
Nooowkie <alirahmanzanov2323@gmail.com>
2026-01-09 12:45:38 +0600

    hm-3

--- js/lesson.js
+++ js/lesson.js
@@ -15,37 +15,46 @@ phoneButton.onclick = () => {
 }
 
 //Tab slider
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+const tabs = document.querySelectorAll('.tab_content_item')
+const tabParent = document.querySelector('.tab_content_items')
 
-const tabContentBlocks = document.querySelectorAll('.tab_content_block');
-const tabContentItems = document.querySelectorAll('.tab_content_item');
-const tabContentItemsParent = document.querySelector('.tab_content_items');
+let index = 0
 
-const hideTabContent = () => {
-    tabContentBlocks.forEach((block) => {
-        block.style.display = 'none';
-    });
-    tabContentItems.forEach((item) => {
-        item.classList.remove('tab_content_item_active');
-    });
-}; 
-
-const showTabContent = (i = 0) => {
-    tabContentBlocks[i].style.display = 'block';
-    tabContentItems[i].classList.add('tab_content_item_active');
+const hideSlide = () => {
+    tabContentBlocks.forEach(item => item.style.display = 'none')
+    tabs.forEach(item => item.classList.remove('tab_content_item_active'))
 }
 
-hideTabContent();
-showTabContent();
+const showSlide = (i = 0) => {
+    tabContentBlocks[i].style.display = 'block'
+    tabs[i].classList.add('tab_content_item_active')
+}
 
-tabContentItemsParent.onclick = (event) => {
+const autoSlider = (i = 0) => {
+    setInterval(() => {
+        i++
+        if (i > tabContentBlocks.length - 1) {
+            i = 0
+        }
+        index = i
+        hideSlide()
+        showSlide(i)
+    }, 3000) 
+}
+
+tabParent.onclick = (event) => {
     if (event.target.classList.contains('tab_content_item')) {
-        tabContentItems.forEach((item, index) => {
-            if (event.target === item) {
-                hideTabContent();
-                showTabContent(index);
-                
+        tabs.forEach((tab, idx) => {
+            if (event.target === tab) {
+                index = idx
+                hideSlide()
+                showSlide(index)
             }
-        });
+        })
     }
-};
+}
 
+hideSlide()
+showSlide(index)
+autoSlider(index)
